8ee1548b20bb73c8e278727b535f9f83fbe70231
Sven <sven@macs-MBP.fritz.box>
2017-01-21 19:59:19 +0100

    Solution for week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Functions that are able to cache the inverse matrix
+## According to the function call of makeCacheMatrix the different
+## forms of subsequent function calls of the list members are executed 
+## e. g. setInverseMatrix, getInverseMatrix ...
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ inverse <- NULL
+ set <- function (y) {
+   x <<- y
+   inverse <<- NULL
+ }
+ get <- function () x
+ setInverseMatrix <- function (solveMatrix) inverse <<- solveMatrix
+ getInverseMatrix <- function () inverse
+ list (set=set, get=get, setInverseMatrix = setInverseMatrix, getInverseMatrix=getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## This function returnes the inverse matrix given by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getInverseMatrix()
+        if(!is.null(inverse)) {
+          message("lookup cached data")
+          return (inverse)
+        }
+        data <- x$get()
+        inverse <- solve (data)
+        x$setInverseMatrix(inverse)
+        inverse
 }
